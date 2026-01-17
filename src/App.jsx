d8005bc257e137d2d93ace1f004320ca0b9c98a1
Tejj <tejjjphani@gmail.com>
2026-01-17 11:18:28 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>My Dummy React Vite Project</h1>
+      <p>This is a simple dummy app created for GitHub + GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
